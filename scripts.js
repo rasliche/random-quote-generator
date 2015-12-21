99a5da9d6e00ec3e46c27a6f379e241bbb18976a
EricR <rasliche@gmail.com>
2015-12-21 17:44:17 -0500

    Quote library variable storing quotes
    Not sure if this is a good idea yet, but I assume it is. Let’s see.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var quoteLibrary = [
+  ["A ship in harbor is safe, but that is not what ships are built for.", ""],
+  ["The dangerous man is the one who has only one idea, because he'll fight and die for it. The way real science goes is that you come up with lots of ideas, and most of them will be wrong.", "Francis Crick"],
+  ["Everyone wants to live on top of the mountain, but not very many want to climb to get there", ""],
+  ["Terrible ideas, reprehensible ideas, do not disappear if you ban them, they go underground. They acquire a kind of glamour of taboo. In the harsh light of day, they are out there and, like vampires, they die in the sunlight", "Salmon Rushdie"],
+  ["The shackles that hold us down aren't really staked to the ground, so shake them off and watch them disappear", "Larry, and His Flask"],
+  ["It's your adventure.", "Mates"],
+  ["The difference between ordeal and adventure is attitude",""],
+  ["Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.", "Martin Luther King, Jr."],
+  ["Fish on!", "Hoyt"],
+  ["Keep your rod tip up!", "Hoyt"],
+  ["Stay tight!", "Hoyt"]
+];
